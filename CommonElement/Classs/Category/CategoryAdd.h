e38c9c945b31c69225517263331f9b536fd02e1c
熊彬 <coderBear@163.com>
2016-06-06 15:00:50 +0800

    add heaer.h

---
+++ CommonElement/Classs/Category/CategoryAdd.h
@@ -0,0 +1,36 @@
+//
+//  CategoryAdd.h
+//  CommonElement
+//
+//  Created by 熊彬 on 16/6/6.
+//  Copyright © 2016年 熊彬. All rights reserved.
+//
+
+#ifndef CategoryAdd_h
+#define CategoryAdd_h
+
+// Foundation
+#import "NSObject+BinAdd.h"
+#import "NSDate+BinAdd.h"
+#import "NSNotificationCenter+BinAdd.h"
+#import "NSArray+BinAdd.h"
+#import "NSString+BinAdd.h"
+#import "NSData+BinAdd.h"
+#import "NSDictionary+BinAdd.h"
+#import "NSMutableArray+BinAdd.h"
+#import "NSMutableDictionary+BinAdd.h"
+#import "NSNumber+BinAdd.h"
+#import "NSDecimalNumber+BinAdd.h"
+
+// UIKit
+#import "UIColor+BinAdd.h"
+#import "UIGestureRecognizer+BinAdd.h"
+#import "UIImage+BinAdd.h"
+#import "UILabel+FTDLabelExtentions.h"
+#import "UITextField+FFKeyBoard.h"
+#import "UITextField+BinAdd.h"
+#import "UIView+FrameExtention.h"
+#import "UIControl+BinAdd.h"
+
+
+#endif /* CategoryAdd_h */
